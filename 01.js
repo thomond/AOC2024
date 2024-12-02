2e6d03d007eac3cdc47e80b8a1332fa19a61b94a
thomond <john.quirke@gmail.com>
2024-12-02 00:07:54 +0000

    Create 01.js
    initial

---
+++ 01.js
@@ -0,0 +1,8 @@
+// Get input data from source
+const response = await fetch("https://adventofcode.com/2024/day/1/input"); 
+const input = await response.text();
+
+const lines = input.split("/n");
+for(const line of lines){
+  	const
+}
